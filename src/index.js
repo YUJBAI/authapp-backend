85d7878762bfdc3677384d906efeb5a146bf7966
YUJBAI <yujie.bai@student.unimelb.edu.au>
2025-01-19 17:01:28 +1100

    add v1 routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const authRouter = require("./routes/auth.router");
+//const authRouter = require("./routes/auth.router");
+const v1Router = require("./routes");
 const config = require("./utils/config");
 const express = require("express");
 
 const app = express();
 
-app.use("/auth", authRouter);
+//app.use("/auth", authRouter);
+app.use("/v1", v1Router);
 
 app.listen(config.PORT, () => {
   console.log(`server listening on port ${config.PORT}`);
